2e78525328b75f5c990734c7f2ef2f3be377f96f
Pedro Furtado <pedro.felipe.azevedo.furtado@gmail.com>
2021-07-23 20:49:44 -0300

    Initial test only with CentOS 8

--- test/test_with_docker.rb
+++ test/test_with_docker.rb
@@ -7,7 +7,7 @@ end
 class WithDockerTest < Minitest::Test
   # Run code before a group of test (see: https://github.com/seattlerb/minitest#how-to-run-code-before-a-group-of-tests)
   SETUP = begin
-    `docker-compose build` unless macos?
+    `docker-compose build --no-cache` unless macos?
   end
 
   #def test_centos_6
@@ -18,29 +18,29 @@ class WithDockerTest < Minitest::Test
   #  test with: 'centos_7'
   #end
 
-  #def test_centos_8
-  #  test with: 'centos_8'
+  def test_centos_8
+   test with: 'centos_8'
+  end
+
+  #def test_debian_9
+  #  test with: 'debian_9'
   #end
 
-  def test_debian_9
-    test with: 'debian_9'
-  end
+  #def test_debian_10
+  #  test with: 'debian_10'
+  #end
 
-  def test_debian_10
-    test with: 'debian_10'
-  end
+  #def test_with_ubuntu_16
+  #  test with: 'ubuntu_16.04'
+  #end
 
-  def test_with_ubuntu_16
-    test with: 'ubuntu_16.04'
-  end
+  #def test_with_ubuntu_18
+  #  test with: 'ubuntu_18.04'
+  #end
 
-  def test_with_ubuntu_18
-    test with: 'ubuntu_18.04'
-  end
-
-  def test_with_ubuntu_20
-    test with: 'ubuntu_20.04'
-  end
+  #def test_with_ubuntu_20
+  #  test with: 'ubuntu_20.04'
+  #end
 
   #def test_with_deepin
   #  test with: 'deepin'
@@ -50,9 +50,9 @@ class WithDockerTest < Minitest::Test
   #  test with: 'archlinux'
   #end
 
-  def test_with_macos
-    assert_equal `bin/wkhtmltopdf --version`.strip, 'wkhtmltopdf 0.12.6 (with patched qt)' if macos?
-  end
+  #def test_with_macos
+  #  assert_equal `bin/wkhtmltopdf --version`.strip, 'wkhtmltopdf 0.12.6 (with patched qt)' if macos?
+  #end
 
   private
 
